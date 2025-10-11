c9c8457d88a472fc62bc8a9f8d1b988d8c4ee2cd
Aditya Prakash <coderadi72@gmail.com>
2025-10-11 19:24:45 +0000

    updated

--- tut2.cpp
+++ tut2.cpp
@@ -1,36 +1,60 @@
-#include<iostream>
-using namespace std;
+#include <iostream>
+#include <vector>
 
-int main()
-    {
-      int a,d,sum, age ,year,day;//integer bass no. with no decimal or hum log agee mea - or + bhi laga sakte ha 4 byte ka space hi hoat ha is ka range (-2147483648 sa 2147483647 basa) 
+
+using namespace std;
+typedef string lol;
+using king = char;
+
+int main ()
+{
+     
+       int a,d,sum, age ,year,day;//integer bass no. with no decimal or hum log agee mea - or + bhi laga sakte ha 4 byte ka space hi hoat ha is ka range (-2147483648 sa 2147483647 basa) 
       float price,r,k;//decimal
       char x,f,e,qw,p;//character if we put some thing other than i we will get ascii value or if we add it with integer it will use the character assinged value for the character hum log koi bhi character likh sakte ha compiller us ko libreary sa feach kar lega par wo ('') ma aa jata ha bina single cotatin ka nhi hoga ya code runn ma error show kare ga 
       bool m,bro,apple,pie;//only store true or false value kyu ki is ka ka space ek bit hi hota ha ya 0 or ya 1 hoga.
-      string name1= "hero@",name2="baba#",name3="car maa mare harsh ka gaar",inf="99 9";//it is a collection of character in which it contain letter ,digits ,symboles,and space but we can't write any fxn from the std liberery because it is alredy in the program so it will show error
+      king hi ='%';
+      
+      
+      
+      lol my_name ="aditya"                    ;
+      string name1= "hero@",name2="baba#",name3="car",inf="999";//it is a collection of character in which it contain letter ,digits ,symboles,and space but we can't write any fxn from the std liberery because it is alredy in the program so it will show error
+      
       a = 99;
       d = 123; 
+     
       age=16;
       year=2025;
       day=1.8;
-    sum =a+d;
+   
+      sum =a+d;
      price =6767.98;
      r=76.67;
+    
      k=89.97532345678;
     x='1';
     f='l';
+  
     e='b';
 qw='%';
 p='a';
 m=true;
-bro=true;
+            
+              bro=true;
 apple=false;
+         
 pie=false;
-cout<<bro<<endl;
-cout<<apple<<endl;
-cout<<pie<<endl;
-cout<<"i can eat "<<inf<<" chicken"<<endl;
+
+
 cout<<name1<<endl;
+      cout<<hi<<endl;
+
+                 cout<<my_name<<endl;   
+cout<<bro<<endl;
+              cout<<apple<<endl;
+                cout<<pie<<endl;
+     cout<<"i can eat "<<inf<<" chicken"<<endl;
+          cout<<name1<<endl;
 cout<<name2<<endl;  
 cout<<name3<<endl; 
 cout<<m<<endl; 
