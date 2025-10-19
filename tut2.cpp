a8359d15927791950332ba877a671ef0c01d9925
Aditya Prakash <coderadi72@gmail.com>
2025-10-19 07:15:10 +0000

    update

--- tut2.cpp
+++ tut2.cpp
@@ -8,19 +8,11 @@ using king = char;
 
 
 
-
-
-
-
-
- 
-
-
 int main ()
 {
      
-       int a,d,sum, age ,year,day,u, l;//integer bass no. with no decimal or hum log agee mea - or + bhi laga sakte ha 4 byte ka space hi hoat ha is ka range (-2147483648 sa 2147483647 basa) 
-      float price,umi,book,r,k,fan, cake,fcuk, tree,jam;//decimal
+       int a,d,sum,price,discount, age ,year,day,u, l;//integer bass no. with no decimal or hum log agee mea - or + bhi laga sakte ha 4 byte ka space hi hoat ha is ka range (-2147483648 sa 2147483647 basa) 
+      float pri,sale_price,umi,book,r,k,fan, cake,fcuk, tree,jam;//decimal
       char x,mr,f,e,qw,p;//character if we put some thing other than i we will get ascii value or if we add it with integer it will use the character assinged value for the character hum log koi bhi character likh sakte ha compiller us ko libreary sa feach kar lega par wo ('') ma aa jata ha bina single cotatin ka nhi hoga ya code runn ma error show kare ga 
       bool m,bro,apple,pie;//only store true or false value kyu ki is ka ka space ek bit hi hota ha ya 0 or ya 1 hoga.
       king hi ='%';
@@ -30,7 +22,7 @@ int main ()
       lol my_name ="aditya"                    ;
       string name1= "hero@",name2="baba#",name3="car",inf="999";//it is a collection of character in which it contain letter ,digits ,symboles,and space but we can't write any fxn from the std liberery because it is alredy in the program so it will show error
       mr=(bool)10;//yaha hum log inplicet use kar rahe ha implesit msa hum log koi bhi variable ko kise or data type ka ho us ko hum log kise or data type ma use kar sakta ha jise hum log int data type ka variable ko boollian data type ma use kar sakte ha 
-      
+       sale_price=price/(float)discount*100;
       a = 99;
       d = 123; 
      u=9033;
@@ -39,7 +31,7 @@ int main ()
       day=1.8;
    l=234;
       sum =a+d;
-     price =6767.98;
+     pri =6767.98;
      r=76.67;
     
      k=89.97532345678;
@@ -103,5 +95,14 @@ cout<<book<<endl;
 cout<<cake<<endl;
     cout<<fan<<endl;
              cout<<jam<<endl;
+          cout<<"please enter your orignal price"<<endl; 
+             cin>>price;
+        cin>>discount;
+       cout<<sale_price<<endl;
+        
+
+
+
+
    return 0 ;
   } 
